2f69fba8846bb88e1e5e79e213bec1f4d36e248f
brianbryank <brianbryan0125@gmail.com>
2022-06-14 13:14:49 +0300

    up[date

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,7 +2,7 @@
 
 const req = require('request');
 const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/4/';
+const url = 'https://swapi-api.hbtn.io/api/films/';
 req.get(url + id, function (error, res, body) {
   if (error) {
     console.log(error);
